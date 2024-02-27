2774710d76abd29c55348a342724442420204f5a
fex <judfux@gmail.com>
2024-02-27 22:22:48 +0100

    added click animation to player card

--- app/src/main/java/com/fexapk/aoelookup/screens/PlayerComposables.kt
+++ app/src/main/java/com/fexapk/aoelookup/screens/PlayerComposables.kt
@@ -1,6 +1,9 @@
 package com.fexapk.aoelookup.screens
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,8 +20,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -37,8 +45,20 @@ fun PlayerCard(
     player: Player,
     modifier: Modifier = Modifier
 ) {
+
+    var isTouched by remember { mutableStateOf(false) }
+
+    val alphaValue by animateFloatAsState(
+        targetValue = if (isTouched) 0.5f else 1f,
+        animationSpec = tween(durationMillis = 300),
+        label = "Player card touched"
+    )
+
     OutlinedCard(
-        modifier = Modifier.height(80.dp)
+        modifier = Modifier
+            .clickable { isTouched = !isTouched }
+            .alpha(alphaValue)
+            .height(80.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
